9693c106cb22e6bdd6f5105f52bcda752f858c4b
Chinmay-G <chinmay.the1@gmail.com>
2025-03-13 22:07:14 +0530

    Almost done..

--- main.js
+++ main.js
@@ -12,7 +12,52 @@ function reverseString(str) {
         .forEach(letter => arr.unshift(letter))
     return arr.join('');
 }
-console.log(reverseString('arya Khan'));
-// exports.reverseString = reverseString;
-// exports.capitalize = capitalize;
-module.exports = { capitalize, reverseString };
+
+const calculator = {
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    divide: (a, b) => a / b,
+    multiply: (a, b) => a * b,
+}
+
+function caesarCipher(word, shift) {
+    const alphabetsL = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    const alphabetsU = alphabetsL.map(el => el.toUpperCase());
+    const cipheredWord = word
+        .split('')
+        .map(letter => {
+            if (!isAlphabet(letter) || letter === ' ')
+                return letter;
+            if (letter === letter.toUpperCase()) {
+                return caesarCipherShifter(alphabetsU, letter, shift);
+            }
+            if (letter === letter.toLowerCase()) {
+                return caesarCipherShifter(alphabetsL, letter, shift);
+            }
+            else return letter;
+        })
+        .join('');
+
+    return cipheredWord;
+}
+
+function caesarCipherShifter(alphabets, letter, shift) {
+    const index = alphabets.indexOf(letter);
+
+    if (index + shift > alphabets.length - 1)
+        return alphabets[(index - alphabets.length) + shift]
+    return alphabets[index + shift];
+}
+
+function isAlphabet(str) {
+    return /^[a-zA-Z()]+$/.test(str);
+}
+
+const arr = ['a', 'b', 'c'];
+console.log(arr.indexOf('b'));
+
+console.log(caesarCipher('abc', 3));
+
+console.log(caesarCipher('Hello, World!', 3));
+
+module.exports = { capitalize, reverseString, calculator, caesarCipher };
--- main.test.js
+++ main.test.js
@@ -1,10 +1,8 @@
 // const { default: test } = require('node:test');
 // const { describe } = require('yargs');
-const { capitalize, reverseString } = require('./main');
-// import { capitalize } from './main.js';
-
-// const capitalize = require('./main');
-// // const reverseString = require('./main');
+// const { default: test, it } = require('node:test');
+const { capitalize, reverseString, calculator, caesarCipher } = require('./main');
+// const { describe } = require('yargs');
 
 describe('capitalizes the first letter', () => {
     it('test - 1', () => {
@@ -30,6 +28,40 @@ describe('Reverses the string', () => {
     })
 })
 
-// test('capitalizes the first letter', () => {
-//     expect(capitalize(3).toBe(4));
-// })
+describe('Calculator works', () => {
+    it('add test - 1', () => {
+        expect(calculator.add(2, 3)).toBe(5);
+    })
+    it('add test - 2', () => {
+        expect(calculator.add(31, 369)).toBe(400);
+    })
+    it('add test - 3', () => {
+        expect(calculator.add(31.2, 369.22)).toBeCloseTo(400.42);
+    })
+
+    it('subtract test - 1', () => {
+        expect(calculator.subtract(31.2, 369.22)).toBeCloseTo(-338.02);
+    })
+
+    it('divide test - 1', () => {
+        expect(calculator.divide(31.2, 369.22)).toBeCloseTo(0.08450246466);
+    })
+    it('multiply test - 1', () => {
+        expect(calculator.multiply(31.2, 369.22)).toBeCloseTo(11519.664);
+    })
+})
+
+describe('Caesar Ciphers the text', () => {
+    it('test - 1', () => {
+        expect(caesarCipher('abc', 3)).toMatch(/def/);
+    })
+    it('test - 2', () => {
+        expect(caesarCipher('xyz', 3)).toMatch(/abc/);
+    })
+    it('test - 3', () => {
+        expect(caesarCipher('HeLLo', 3)).toMatch(/KhOOr/);
+    })
+    it('test - 4', () => {
+        expect(caesarCipher('Hello, World!', 3)).toMatch(/Khoor, Zruog!/);
+    })
+})
